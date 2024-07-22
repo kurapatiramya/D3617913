97616bc2674dcbf482ab107c40fbc2dbd510a6e6
kurapatiramya <kurapatiramya01@gmail.com>
2024-07-23 00:29:09 +0530

    Commit 7

--- app/src/main/java/uk/ac/tees/mad/d3617913/ui/AddEditItemScreen.kt
+++ app/src/main/java/uk/ac/tees/mad/d3617913/ui/AddEditItemScreen.kt
@@ -23,11 +23,13 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -36,6 +38,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusDirection
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalFocusManager
@@ -57,7 +60,7 @@ import kotlinx.coroutines.tasks.await
 import java.io.File
 import java.util.UUID
 
-@OptIn(ExperimentalPermissionsApi::class)
+@OptIn(ExperimentalPermissionsApi::class, ExperimentalMaterial3Api::class)
 @Composable
 fun AddEditItemScreen(navController: NavController, itemId: String? = null) {
     val context = LocalContext.current
@@ -112,14 +115,19 @@ fun AddEditItemScreen(navController: NavController, itemId: String? = null) {
             db.collection("categories").get().await().documents.map { it.getString("name") ?: "" }
 
     }
-    Scaffold() { innerPadding ->
-        Box(modifier = Modifier.padding(innerPadding)) {
-            Column(modifier = Modifier.padding(16.dp)) {
+    Scaffold(
+        topBar = {
+            TopAppBar(title = {
                 Text(
                     text = "Add/Edit Item",
-                    fontSize = 24.sp,
-                    modifier = Modifier.padding(bottom = 16.dp)
+                    fontSize = 24.sp
                 )
+            })
+        }
+    ) { innerPadding ->
+        Box(modifier = Modifier.padding(innerPadding)) {
+            Column(modifier = Modifier.padding(16.dp)) {
+
 
                 TextField(
                     value = name,
@@ -300,7 +308,10 @@ fun AddEditItemScreen(navController: NavController, itemId: String? = null) {
                         .fillMaxWidth()
                 ) {
                     if (isLoading) {
-                        CircularProgressIndicator(modifier = Modifier.size(24.dp))
+                        CircularProgressIndicator(
+                            modifier = Modifier.size(24.dp),
+                            color = Color.White
+                        )
                     } else {
                         Text(text = "Save")
                     }
--- app/src/main/java/uk/ac/tees/mad/d3617913/ui/HomeScreen.kt
+++ app/src/main/java/uk/ac/tees/mad/d3617913/ui/HomeScreen.kt
@@ -1,19 +1,26 @@
 package uk.ac.tees.mad.d3617913.ui
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Category
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
@@ -21,16 +28,45 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
 import androidx.navigation.NavHostController
+import com.google.firebase.Firebase
+import com.google.firebase.firestore.firestore
+import kotlinx.coroutines.tasks.await
 import uk.ac.tees.mad.d3617913.Screen
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(navController: NavHostController) {
+    val db = Firebase.firestore
+    var groceryLists by remember { mutableStateOf(listOf<GroceryList>()) }
+    var isLoading by remember { mutableStateOf(true) }
+
+    LaunchedEffect(Unit) {
+        isLoading = true
+        groceryLists = db.collection("items").get().await().documents.map { document ->
+            GroceryList(
+                id = document.id,
+                name = document.getString("name") ?: "",
+                category = document.getString("category") ?: "",
+                imageLink = document.getString("imageUri") ?: "",
+                notes = document.getString("notes") ?: "",
+                quantity = document.getString("quantity") ?: ""
+            )
+        }
+        isLoading = false
+    }
+
     Scaffold(topBar = {
         TopAppBar(title = {
             Text(
@@ -54,7 +90,7 @@ fun HomeScreen(navController: NavHostController) {
                 })
             NavigationBarItem(
                 selected = false,
-                onClick = {  navController.navigate(Screen.Profile.route)  },
+                onClick = { navController.navigate(Screen.Profile.route) },
                 icon = { Icon(imageVector = Icons.Default.Person, contentDescription = null) },
                 label = {
                     Text(text = "Profile")
@@ -66,7 +102,55 @@ fun HomeScreen(navController: NavHostController) {
         Column(
             Modifier.padding(innerPadding)
         ) {
-            Text(text = "Home")
+
+            Column(modifier = Modifier.padding(16.dp)) {
+                Text(
+                    text = "Your Grocery Lists",
+                    style = MaterialTheme.typography.titleMedium,
+                    modifier = Modifier.padding(bottom = 16.dp)
+                )
+                if (isLoading) {
+                    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                        CircularProgressIndicator()
+                    }
+                } else if (groceryLists.isEmpty()) {
+                    Text(text = "No grocery lists found.")
+                } else {
+                    LazyColumn(modifier = Modifier.fillMaxSize()) {
+                        items(groceryLists) { groceryList ->
+                            GroceryListItem(groceryList, navController)
+                        }
+                    }
+                }
+            }
         }
     }
 }
+
+
+@Composable
+fun GroceryListItem(groceryList: GroceryList, navController: NavController) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 8.dp)
+            .clickable {
+
+            },
+        elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
+    ) {
+        Column(modifier = Modifier.padding(16.dp)) {
+            Text(text = groceryList.name, style = MaterialTheme.typography.titleSmall)
+            Text(text = groceryList.quantity, style = MaterialTheme.typography.bodyMedium)
+        }
+    }
+}
+
+data class GroceryList(
+    val id: String,
+    val name: String,
+    val category: String,
+    val imageLink: String,
+    val notes: String,
+    val quantity: String,
+)
